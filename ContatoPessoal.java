1db7f55826e6e9f173979f569737c865438f6f41
Yago498 <iagoarthur755@gmail.com>
2026-07-10 14:01:20 -0300

    Update ContatoPessoal.java

--- ContatoPessoal.java
+++ ContatoPessoal.java
@@ -1,1 +1,32 @@
+package model;
 
+public class ContatoPessoal extends Contato implements Favoritavel {
+
+    private boolean favorito;
+
+    public ContatoPessoal(String nome, String telefone, boolean favorito) {
+        super(nome, telefone);
+        this.favorito = favorito;
+    }
+
+    @Override
+    public String getTipo() {
+        return "Pessoal";
+    }
+
+    @Override
+    public void favoritar() {
+        this.favorito = true;
+    }
+
+    @Override
+    public boolean isFavorito() {
+        return favorito;
+    }
+
+    @Override
+    public String toString() {
+        String estrela = favorito ? " ★" : "";
+        return super.toString() + estrela;
+    }
+}
